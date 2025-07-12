bf685498b860333c77b045c071556553fcc3e6f7
Насибуллин Данил <d.nasibullin@freshauto.ru>
2025-07-12 20:22:45 +0300

    рефакторинг send_icmp/operation

--- app/domain/checks/send_icmp/operation.rb
+++ app/domain/checks/send_icmp/operation.rb
@@ -13,15 +13,10 @@ module IpMonitoring
           def call(address)
             step validate(address: address)
 
-            Hanami.logger.debug "[Thread: #{Thread.current.object_id}] [SendICMP] Address: #{address}"
             ping = Net::Ping::External.new(address, nil, settings.icmp_timeout)
             success = ping.ping?
 
-            Hanami.logger.debug "[Thread: #{Thread.current.object_id}] [SendICMP] Address: #{address} - #{success}"
             { failed: !success, rtt_ms: ping.duration && ping.duration * 1000 }
-          rescue NameError => e
-            Hanami.logger.error(e.message)
-            Failure[:internal_server_error, e.message]
           end
         end
       end
