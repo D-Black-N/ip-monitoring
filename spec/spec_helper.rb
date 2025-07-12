2b38449496a923544758eb2d9abea9b49f9c7941
Насибуллин Данил <d.nasibullin@freshauto.ru>
2025-07-12 20:28:55 +0300

    добавлен net/ping в тесты

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ SPEC_ROOT = Pathname(__dir__).realpath.freeze
 
 ENV["HANAMI_ENV"] ||= "test"
 require "hanami/prepare"
+require 'net/ping'
 require_relative 'support/factories'
 
 SPEC_ROOT.glob("support/**/*.rb").each { |f| require f }
